d05d030fbba0c021a2c86aade9baa2a183a9d901
fauzanfebrian <fauzanfebriann@gmail.com>
2025-08-18 21:48:11 +0700

    malloca change

---
+++ section-7/malloc.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    double *bArr;
+    int arraySize;
+
+    scanf("%d", &arraySize);
+    bArr = (double *)malloc(sizeof(double) * 2);
+    if (bArr != NULL) {
+        printf("Allocation, Succeeded");
+    }
+
+    ("Allocation Failed");
+}
