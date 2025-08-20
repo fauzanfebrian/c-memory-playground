83c1f21b92c6b1c5bed6ff22c5fc745ac39e1ec4
fauzanfebrian <fauzanfebriann@gmail.com>
2025-08-20 21:43:04 +0700

    calloc+free

---
+++ section-7/calloc.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    int arraySize;
+
+    printf("Input the size of the array: ");
+    scanf("%d", &arraySize);
+
+    int *a = (int *)calloc(arraySize, sizeof(int));
+
+    printf("First index: %d", *a);
+
+    free(a);
+}
