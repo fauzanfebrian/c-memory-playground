19dce588bfa2a15ab4d620f4527df68adafceae6
Fauzan <fauzanfebriann@gmail.com>
2025-11-22 21:47:32 +0700

    section-23: consts

---
+++ src/section-23/consts.c
@@ -0,0 +1,13 @@
+
+
+int main() {
+    const age = 35;  // const into an age
+
+    const int* ptr;  // a pointer to a constant
+
+    int* const ptr1;  // a constant pointer to the int variable
+
+    const int* const ptr2;  // a constant pointer pointing to a constant intefer
+
+    return 0;
+}
