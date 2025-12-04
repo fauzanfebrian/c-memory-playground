999d86d166e6e13ecd2afc97512c5c477b5a979f
Fauzan <fauzanfebriann@gmail.com>
2025-12-04 21:40:18 +0700

    section-25: add enabler bit

--- src/section-26/control-register-bit-field.c
+++ src/section-26/control-register-bit-field.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
 
 typedef struct ControlRegister {
-    unsigned int enableDevice : 1;
-    unsigned int resetDevice : 1;
-    unsigned int interruptDevice : 1;
-    unsigned int modeSelect : 2;
-    unsigned int reserved : 27;
+    unsigned int enableDevice : 1;     // Bit 0: Enable/Disable the device
+    unsigned int resetDevice : 1;      // Bit 1: Reset the device
+    unsigned int interruptDevice : 1;  // Bit 2: enable or disable interrupts
+    unsigned int modeSelect : 2;       // Bits 3-4: select operating mode
+    unsigned int enableTX : 1;         // Bit 5
+    unsigned int enableRX : 1;         // Bit 6
+    unsigned int reserved : 27;        // Bits 7-31: Reserved for future use
 } ControlRegister;
 
 int main() {
